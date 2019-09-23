c099c4a12616d137d5586abc6d4198145419974e
Emily Yu <emilyyu518@gmail.com>
2019-09-23 13:27:43 -0500

    creates basic main view components

---
+++ src/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Heading } from '../common/styles';
+
+function Dashboard() {
+  return (
+    <div>
+      <Heading>Dashboard</Heading>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Heading } from '../common/styles';
+import colors from '../common/colors';
+
+function Home() {
+  return (
+    <div>
+      <Heading color={colors.yellow} textShadowColor={colors.vDarkPurple}>Operation Spark Alumni Network</Heading>
+    </div>
+  );
+}
+
+export default Home;
