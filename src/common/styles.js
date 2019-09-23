50dd7e38aacb736fd2cbbf37b664f5f1831d5c4d
Emily Yu <emilyyu518@gmail.com>
2019-09-23 13:25:56 -0500

    adds heading to common styles

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import colors from './colors';
+
 const Row = styled.div`
   display: flex;
 `;
@@ -9,7 +11,15 @@ const Column = styled.div`
   flex-direction: column;
 `;
 
+const Heading = styled.h1`
+  color: ${({ color }) => color ? color : colors.white};
+  font-family: Atami-Bold;
+  font-size: ${({ fontSize }) => fontSize ? fontSize : '3.5rem'};
+  text-shadow: .25rem .25rem 0 ${({ textShadowColor }) => textShadowColor ? textShadowColor : colors.translucentLightPurple};
+`;
+
 export {
   Row,
   Column,
+  Heading,
 };
