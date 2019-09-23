e17974e44cfac798c7343dbe878d696d9be1e54e
Emily Yu <emilyyu518@gmail.com>
2019-09-23 13:07:26 -0500

    creates basic styling utilities

---
+++ src/common/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  lightYellow: '#FFE367',
+  yellow: '#F3C700',
+  white: '#FFFFFF',
+  lightPurple: '#BB7CE5',
+  purple: '#9445CA',
+  darkPurple: '#5F2C82',
+  vDarkPurple: '#2E1937',
+  translucentLightPurple: '#BB7CE560'
+};
+
+export default colors;
---
+++ src/common/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export {
+  Row,
+  Column,
+};
