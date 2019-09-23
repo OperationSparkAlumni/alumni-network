7c960cccff644f140c2b5026a65e0399698e974f
Emily Yu <emilyyu518@gmail.com>
2019-09-23 13:35:06 -0500

    replaces "dashboard" heading with personalized user greeting

--- src/Dashboard/index.jsx
+++ src/Dashboard/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { useAuthContext } from '../common/AuthContext';
 import { Heading } from '../common/styles';
 
 function Dashboard() {
+  const { userData } = useAuthContext();
+  const { nameFirst, nameLast } = userData;
+
   return (
     <div>
-      <Heading>Dashboard</Heading>
+      <Heading>Hello, {`${nameFirst} ${nameLast}`}!</Heading>
     </div>
   );
 }
