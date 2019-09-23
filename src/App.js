f433d6e375419f214f914108e2a446313e748c1f
Emily Yu <emilyyu518@gmail.com>
2019-09-23 13:34:37 -0500

    creates basic app layout with header, footer, and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Layout from './components/Layout';
+import { AuthProvider } from './common/AuthContext';
+import PublicRoutes from './common/PublicRoutes';
+import ProtectedRoutes from './common/ProtectedRoutes';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Layout>
+        <PublicRoutes />
+        <ProtectedRoutes />
+      </Layout>
+    </AuthProvider>
   );
 }
 
