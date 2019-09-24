3f3683be02aaeae29efd873fd3fb35741f972c56
Emily Yu <emilyyu518@gmail.com>
2019-09-24 17:23:26 -0500

    makes style utilities more customizable via props

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,21 +1,28 @@
 import styled from 'styled-components';
 
-import colors from './colors';
-
 const Row = styled.div`
+  align-items: ${({ alignItems }) => alignItems};
   display: flex;
+  justify-content: ${({ justifyContent }) => justifyContent};
+  margin: ${({ margin }) => margin};
+  padding: ${({ padding }) => padding};
 `;
 
 const Column = styled.div`
+  align-items: ${({ alignItems }) => alignItems};
   display: flex;
   flex-direction: column;
+  justify-content: ${({ justifyContent }) => justifyContent};
+  margin: ${({ margin }) => margin};
+  padding: ${({ padding }) => padding};
 `;
 
 const Heading = styled.h1`
-  color: ${({ color }) => color ? color : colors.white};
+  color: ${({ color }) => color};
   font-family: Atami-Bold;
-  font-size: ${({ fontSize }) => fontSize ? fontSize : '3.5rem'};
-  text-shadow: .25rem .25rem 0 ${({ textShadowColor }) => textShadowColor ? textShadowColor : colors.translucentLightPurple};
+  font-size: ${({ fontSize }) => fontSize};
+  margin: ${({ margin }) => margin};
+  text-shadow: .25rem .25rem 0 ${({ textShadowColor }) => textShadowColor};
 `;
 
 export {
