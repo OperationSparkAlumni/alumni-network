7b74b4361cd1e2c8c04e21d642d6e8820cbf9ea1
Emily Yu <emilyyu518@gmail.com>
2019-09-25 14:14:08 -0500

    fixes Atami font import

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fonts/Atami-Bold.otf';
-import './fonts/Atami-Regular.otf';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
