f756b3b0a7bfc68c027e979b320393629d4c14d7
Emily Yu <emilyyu518@gmail.com>
2019-09-30 16:51:00 -0500

    adds dummy data

---
+++ src/mockedData/newsletters.js
@@ -0,0 +1,43 @@
+import anon from '../assets/anon.svg';
+
+const newsletterData = [
+  {
+    id: 1,
+    expanded: true,
+    latest: true,
+    title: 'Really Cool Title',
+    author: {
+        id: '12345678',
+        name: 'Jacqueline Asong',
+        image: anon,
+    },
+    date: 'September, 2019',
+    contents: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse in est ante in nibh mauris cursus mattis. Elementum pulvinar etiam non quam lacus suspendisse. Justo nec ultrices dui sapien eget.\n
+    Odio euismod lacinia at quis. Tellus cras adipiscing enim eu. Tortor dignissim convallis aenean et tortor at. Morbi tincidunt ornare massa eget egestas purus viverra.\n
+    Ornare aenean euismod elementum nisi quis eleifend. Et netus et malesuada fames ac turpis egestas sed tempus. Est ultricies integer quis auctor elit sed. Tempor orci dapibus ultrices in iaculis nunc sed augue. Nunc non blandit massa enim nec dui nunc. Turpis tincidunt id aliquet risus feugiat in ante metus. Ullamcorper morbi tincidunt ornare massa.\n
+    Nisl nunc mi ipsum faucibus vitae. Molestie at elementum eu facilisis sed odio. Sit amet facilisis magna etiam tempor orci eu lobortis elementum. Ultrices gravida dictum fusce ut placerat. Sit amet consectetur adipiscing elit. Sit amet est placerat in egestas erat imperdiet sed. Arcu risus quis varius quam quisque id diam vel. Feugiat nisl pretium fusce id velit ut tortor. Vestibulum lorem sed risus ultricies tristique nulla. Tellus elementum sagittis vitae et leo duis ut diam quam.\n
+    Tempus egestas sed sed risus pretium quam. Malesuada fames ac turpis egestas integer eget aliquet nibh. Non curabitur gravida arcu ac tortor dignissim convallis.`,
+    tags: ['Lorem', 'ipsum', 'dolor', 'sit', 'amet'],
+  },
+  {
+    id: 2,
+    expanded: false,
+    latest: false,
+    title: 'Another Really Cool Title',
+    author: {
+        id: '12345678',
+        name: 'Jacqueline Asong',
+        image: anon,
+    },
+    date: 'August, 2019',
+    preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse in est ante in nibh mauris cursus mattis. Elementum pulvinar etiam non quam lacus suspendisse. Justo nec ultrices dui sapien eget.',
+    contents: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse in est ante in nibh mauris cursus mattis. Elementum pulvinar etiam non quam lacus suspendisse. Justo nec ultrices dui sapien eget.\n
+    Odio euismod lacinia at quis. Tellus cras adipiscing enim eu. Tortor dignissim convallis aenean et tortor at. Morbi tincidunt ornare massa eget egestas purus viverra.\n
+    Ornare aenean euismod elementum nisi quis eleifend. Et netus et malesuada fames ac turpis egestas sed tempus. Est ultricies integer quis auctor elit sed. Tempor orci dapibus ultrices in iaculis nunc sed augue. Nunc non blandit massa enim nec dui nunc. Turpis tincidunt id aliquet risus feugiat in ante metus. Ullamcorper morbi tincidunt ornare massa.\n
+    Nisl nunc mi ipsum faucibus vitae. Molestie at elementum eu facilisis sed odio. Sit amet facilisis magna etiam tempor orci eu lobortis elementum. Ultrices gravida dictum fusce ut placerat. Sit amet consectetur adipiscing elit. Sit amet est placerat in egestas erat imperdiet sed. Arcu risus quis varius quam quisque id diam vel. Feugiat nisl pretium fusce id velit ut tortor. Vestibulum lorem sed risus ultricies tristique nulla. Tellus elementum sagittis vitae et leo duis ut diam quam.\n
+    Tempus egestas sed sed risus pretium quam. Malesuada fames ac turpis egestas integer eget aliquet nibh. Non curabitur gravida arcu ac tortor dignissim convallis.`,
+    tags: ['Lorem', 'ipsum', 'dolor', 'sit', 'amet'],
+  },
+];
+
+export default newsletterData;
