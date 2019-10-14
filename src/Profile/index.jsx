84ccdc934779a6946feeb70d0f0f9b9dd82aac6c
Emily Yu <emilyyu518@gmail.com>
2019-10-14 15:06:23 -0500

    creates basic user profile layout

--- src/Profile/index.jsx
+++ src/Profile/index.jsx
@@ -1,11 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-import { Heading } from '../common/styles';
-import colors from '../common/colors';
+import PersonContainer from '../components/PersonContainer';
+import PeopleContainer from '../components/PeopleContainer';
+import Container from '../components/Container';
+import { Row, Column } from '../common/styles';
 
-const Profile = ({ userId }) => (
-  <Heading>{userId}'s Profile</Heading>
-);
+import alumniProfiles from '../mockedData/alumniProfiles';
+import alumniData from '../mockedData/alumni';
+
+const StyledP = styled.p`
+  padding-bottom: 2rem;
+`;
+
+const Profile = ({ userId }) => {
+  const alum = alumniProfiles[userId];
+  const { image, name, links, quickFacts, description } = alum;
+  const { first, last, preferred } = name;
+
+  return (
+    <Row justifyContent="space-evenly" padding="5rem 0 0">
+      <Column>
+        <PersonContainer heading="Quick Facts" image={image} name={name} links={links} quickFacts={quickFacts} />
+        <PeopleContainer heading={`${preferred}'s Cohort`} people={alumniData} />
+      </Column>
+      <Column>
+        <Container heading={`About ${first} ${last}`} large>
+          <StyledP>{description}</StyledP>
+        </Container>
+      </Column>
+    </Row>
+  );
+};
 
 export default Profile;
