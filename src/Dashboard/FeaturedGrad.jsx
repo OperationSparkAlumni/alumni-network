c5819ef59be0d02decf2d445bc2443187b9c8ffa
Emily Yu <emilyyu518@gmail.com>
2019-10-14 15:01:42 -0500

    refactors basic person container into own component

--- src/Dashboard/FeaturedGrad.jsx
+++ src/Dashboard/FeaturedGrad.jsx
@@ -1,56 +1,57 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import Container from '../components/Container';
-import PersonBubbleExpanded from '../components/PersonBubbleExpanded';
-import colors from '../common/colors';
+import PersonContainer from '../components/PersonContainer';
 import anon from '../assets/anon.svg';
 
-const StyledA = styled.a`
-  color: ${colors.lightYellow};
-  text-align: end;
-`;
-
 const FeaturedGrad = ({ grad }) => {
-  const { image, linkedIn, gitHub, email, name, description, website } = grad;
-  const { displayName, url } = website;
+  const { image, name, links, description } = grad;
   return (
-    <Container heading="Grad of the Month">
-      <PersonBubbleExpanded image={image} linkedIn={linkedIn} gitHub={gitHub} email={email} name={name} />
-      <p>{description}</p>
-      <StyledA href={url} target="_blank" rel="noopener noreferrer">{displayName}</StyledA>
-    </Container>
+    <PersonContainer heading="Featured Grad" image={image} name={name} links={links}>
+      <p>{description}</p>  
+    </PersonContainer>
   );
 };
 
 FeaturedGrad.propTypes = {
   grad: PropTypes.shape({
     image: PropTypes.string,
-    linkedIn: PropTypes.string,
-    gitHub: PropTypes.string,
-    email: PropTypes.string,
-    name: PropTypes.string,
-    description: PropTypes.string,
-    website: PropTypes.shape({
-      displayName: PropTypes.string,
-      url: PropTypes.string,
+    links: PropTypes.shape({
+      linkedIn: PropTypes.string,
+      gitHub: PropTypes.string,
+      email: PropTypes.string,
+      website: PropTypes.shape({
+        displayName: PropTypes.string,
+        url: PropTypes.string,
+      }),
     }),
+    name: PropTypes.shape({
+      first: PropTypes.string,
+      last: PropTypes.string,
+      preferred: PropTypes.string,
+    }),
+    description: PropTypes.string,
   }),
 };
 
 FeaturedGrad.defaultProps = {
   grad: {
     image: anon,
-    linkedIn: 'https://www.linkedin.com/',
-    gitHub: 'https://github.com/',
-    email: 'abc@def.com',
-    name: 'Random Grad',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nSuspendisse in est ante in nibh mauris cursus mattis. Elementum pulvinar etiam non quam lacus suspendisse. Justo nec ultrices dui sapien eget.\n\nOdio euismod lacinia at quis. Tellus cras adipiscing enim eu. Tortor dignissim convallis aenean et tortor at. Morbi tincidunt ornare massa eget egestas purus viverra.',
-    website: {
-      displayName: 'randomgrad.com',
-      url: 'https://www.google.com/',
+    links: {
+      linkedIn: 'https://www.linkedin.com/',
+      gitHub: 'https://github.com/',
+      email: 'abc@def.com',
+      website: {
+        displayName: 'randomgrad.com',
+        url: 'https://www.google.com/',
+      },
     },
+    name: {
+      first: 'Random', 
+      last: 'Grad',
+      preferred: 'Random',
+    },
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nSuspendisse in est ante in nibh mauris cursus mattis. Elementum pulvinar etiam non quam lacus suspendisse. Justo nec ultrices dui sapien eget.\n\nOdio euismod lacinia at quis. Tellus cras adipiscing enim eu. Tortor dignissim convallis aenean et tortor at. Morbi tincidunt ornare massa eget egestas purus viverra.',
   },
 };
 
